411a26b68305bd66b3e2618ebdc0d897eafcdfe9
Oj <oj@oojmed.com>
2021-05-11 19:52:32 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# GT-VSCHighContrast
+
+Theme based on Visual Studio Code's High Contrast theme
+
+## Mods Supported
+
+- Powercord
+- BetterDiscord
+- Vizality
---
+++ gnTheme.mjs
@@ -0,0 +1,5 @@
+export default {
+  name: 'VSC High Contrast',
+  description: 'Theme based on Visual Studio Code\'s High Contrast theme',
+  version: '1.0'
+};
